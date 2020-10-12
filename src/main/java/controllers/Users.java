1fdd8ac0c93a04dd02387e37cb0eab4688cd76ea
Mtownley-02 <93970@farnborough.ac.uk>
2020-10-12 11:07:33 +0100

    Begin to set up apis, have added some data into one of the tables

--- src/main/java/controllers/Users.java
+++ src/main/java/controllers/Users.java
@@ -1,13 +1,11 @@
 package controllers;
 
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import server.Main;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -36,5 +34,26 @@ public class Users{
             System.out.println("Database error: " + exception.getMessage());
             return "{\"Error\": \"Unable to list items.  Error code xx.\"}";
         }
+    }
+    @POST
+    @Path("create")
+    public String UsersCreate(@FormDataParam("UserId") Integer UserId, @FormDataParam("Password") String Password,@FormDataParam("Admin") Boolean Admin,@FormDataParam("AdminId") Integer AdminId, @FormDataParam("SessionToken") Boolean SessionToken){
+        System.out.println("Invoked Users.UsersCreate");
+        try{
+            PreparedStatement ps = Main.db.prepareStatement("INSERT INTO Users (UserId, Password, Admin, AdminId, SessionToken) VALUES (?,?,?,?,?)");
+            ps.setInt(1,UserId);
+            ps.setString(2,Password);
+            ps.setBoolean(3,Admin);
+            ps.setInt(4,AdminId);
+            ps.setBoolean(5,SessionToken);
+        } catch (Exception exception){
+            System.out.println("Database error: " + exception.getMessage());
+            return "{\"Error\": \"Unable to create new item, please see server console for more info.\"}";
+        }
+    }
+    @GET
+    @Path("hub")
+    public String UsersHub(){
+
     }
 }
