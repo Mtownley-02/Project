32d9757b7072b69627ad59645127c1d16fb3c92b
Mtownley-02 <93970@farnborough.ac.uk>
2020-11-15 14:48:02 +0000

    done UserS.html except log list and Login.html, begun on css and js functions to display logs sticky header requires further work

--- resources/client/JS.js
+++ resources/client/JS.js
@@ -1,23 +1,31 @@
-// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
-// Get the header
-var header = document.getElementById("myHeader");
+const navbar = document.getElementById("navbar");
+const sticky = navbar.offsetTop;
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
     } else {
-        header.classList.remove("sticky");
+        navbar.classList.remove("sticky");
     }
 }
 function attemptLogin(){
     console.log("invoked attemptLogin");
-    const UserID=document.getElementById("UserId").value;
-    const url="users/attemptLogin";
+    const formData= new FormData(document.getElementById('Login'));
+    let url="users/attemptLogin";
+    fetch(url, {
+        method: "POST",
+        body: formData,
+    }).then(response =>{
+        return response.json()
+    }).then(response =>{
+        if(response.hasOwnProperty("Error")){
+            alert(JSON.stringify(response));
+        }else{
+            window.open("/client/UserS.html")
+        }
+    })
+
 
 }
