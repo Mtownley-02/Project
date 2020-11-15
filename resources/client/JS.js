d2b417283e82b4008dbf201c26bb726a56d1e565
Mtownley-02 <93970@farnborough.ac.uk>
2020-11-15 15:48:17 +0000

    done UserS.html except log list and Login.html, begun on css and js functions to display logs sticky header functions

--- resources/client/JS.js
+++ resources/client/JS.js
@@ -1,15 +1,4 @@
-window.onscroll = function() {myFunction()};
 
-const navbar = document.getElementById("navbar");
-const sticky = navbar.offsetTop;
-
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
 $(window).scroll(function(){
     if ($(window).scrollTop() >= 300) {
         $('navbar').addClass('fixed-header');
