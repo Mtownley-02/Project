58539de7e647fa71ba712dc9e2424d8418869c6d
Mtownley-02 <93970@farnborough.ac.uk>
2020-11-25 13:18:32 +0000

    done UserS.html except log list and Login.html, begun on css and js functions to display logs sticky header functions begun on api js links, create user page

--- resources/client/js/JS.js
+++ resources/client/js/JS.js
@@ -10,7 +10,7 @@ $(window).scroll(function(){
 function attemptLogin(){
     console.log("invoked attemptLogin");
     const formData= new FormData(document.getElementById('Login'));
-    let url="users/attemptLogin";
+    let url="Users/attemptLogin";
     fetch(url, {
         method: "POST",
         body: formData,
@@ -18,11 +18,28 @@ function attemptLogin(){
         return response.json()
     }).then(response =>{
         if(response.hasOwnProperty("Error")){
-            alert(JSON.stringify(response));
+            console.log(JSON.stringify(response));
         }else{
             window.open("/client/UserS.html")
         }
     })
-
+}
+function goTocreateUser() {
+    window.open("/client.CreateUser.html")
+}
+function createUser(Password,Password1) {
+    console.log("invoked createUser");
+    const formData= new FormData(document.getElementById('Create'));
+    let url="Users/create";
+    if(Password==Password1){
+        fetch(url, {
+            method: "POST",
+            body: formData,
+        }).then(response =>{
+            return response.json()
+        } )
+    }else {
+        console.log('Passwords do not match')
+    }
 
 }
